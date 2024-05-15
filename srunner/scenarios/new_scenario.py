ee76eeca262b6d4a31d68ef7ba6acab25104e008
ysrhee6199 <yongse6199@gmail.com>
2024-05-16 04:20:11 +0900

    update_platoon_scenario

--- srunner/scenarios/new_scenario.py
+++ srunner/scenarios/new_scenario.py
@@ -20,7 +20,8 @@ from srunner.scenariomanager.scenarioatomics.atomic_trigger_conditions import (I
                                                                                InTriggerDistanceToLocation,
                                                                                VelocityPublisher,
                                                                                BrakePublisher,
-                                                                               TimeGapPublisher)
+                                                                               TimeGapPublisher,
+                                                                               TimeOfWaitComparison)
 from srunner.scenariomanager.timer import TimeOut
 from srunner.scenarios.basic_scenario import (BasicScenario,ROSBasicScenario)
 from srunner.tools.scenario_helper import (get_waypoint_in_distance,get_location_in_distance)
@@ -50,6 +51,7 @@ class NewScenario(ROSBasicScenario):
         Setup all relevant parameters and create scenario
         """
         print("Heelo")
+        self.world = world
         self.timeout = timeout
         self.ego_vehicles = ego_vehicles
         self._map = CarlaDataProvider.get_map()
@@ -84,13 +86,16 @@ class NewScenario(ROSBasicScenario):
         sequence.add_child(TimeGapPublisher(name="Publish Timegap", timegap = 0.5))
         sequence.add_child(VelocityPublisher(name="Publish Velocity",target_speed = 14.0))
         sequence.add_child(BrakePublisher(name="Publish Brake",brake=False))
-        sequence.add_child(InTriggerDistanceToLocation(
-            self.ego_vehicles[1], self.trigger_location, self._trigger_dist))
+
+
+        sequence.add_child(TimeOfWaitComparison(duration_time = 5))
         
+
         sequence.add_child(osc_weather_behavior) 
         sequence.add_child(change_road_friction)
-        sequence.add_child(InTriggerDistanceToLocation(
-            self.ego_vehicles[1], self.trigger_end_location,self._trigger_dist))
+        sequence.add_child(SpawnAndDriveVehicle(scenario=self, ego_vehicle=self.ego_vehicles[1], distance_ahead=40))
+        sequence.add_child(TimeOfWaitComparison(duration_time = 20))
+        
         sequence.add_child(BrakePublisher(name="Publish Brake3",brake=True))
         root.add_child(sequence)
         return root
@@ -105,3 +110,80 @@ class NewScenario(ROSBasicScenario):
         Remove all actors upon deletion
         """
         self.remove_all_actors()
+
+    def spawn_vehicle_ahead(self, reference_vehicle, distance):
+        """
+        Spawns a vehicle at a given distance ahead of a reference vehicle.
+        """
+        spawn_transform = reference_vehicle.get_transform()
+        forward_vector = spawn_transform.get_forward_vector()
+
+        # Calculate the new spawn location
+        new_location = spawn_transform.location + forward_vector * distance
+        spawn_transform.location = new_location
+
+        # Ensure there is no collision at the spawn location
+
+        blueprint = self.world.get_blueprint_library().filter('vehicle.*')[0]
+        first_vehicle = CarlaDataProvider.request_new_actor('vehicle.nissan.patrol', spawn_transform)
+        if first_vehicle:
+            self.other_actors.append(first_vehicle)
+            return first_vehicle
+        else:
+            raise RuntimeError("Spawn failed because of collision at spawn position")
+        
+
+class SpawnAndDriveVehicle(py_trees.composites.Sequence):
+    def __init__(self, scenario, ego_vehicle, distance_ahead, name="SpawnAndDriveVehicle"):
+        super(SpawnAndDriveVehicle, self).__init__(name)
+        self.scenario = scenario
+        self.ego_vehicle = ego_vehicle
+        self.distance_ahead = distance_ahead
+        self.vehicle = None
+
+        # Add behavior to wait until vehicle is spawned
+
+    def setup(self, timeout):
+        return True
+
+    def initialise(self):
+        self.vehicle = self.scenario.spawn_vehicle_ahead(self.ego_vehicle, self.distance_ahead)
+        # Add behaviors to control the vehicle's speed after it has been spawned
+        self.add_child(ChangeVehicleSpeed(self.vehicle, 60 / 3.6, duration=5))  # 60 km/h for 5 seconds
+        self.add_child(ChangeVehicleSpeed(self.vehicle, 30 / 3.6, duration=5))  # 30 km/h for 5 seconds
+        self.add_child(ChangeVehicleSpeed(self.vehicle, 80 / 3.6, duration=5))  # 80 km/h for 5 seconds
+        self.add_child(StopVehicle(self.vehicle, brake_value=1.0))  # Stop the vehicle
+
+
+
+class ChangeVehicleSpeed(py_trees.behaviour.Behaviour):
+    def __init__(self, vehicle, speed, duration, name="ChangeVehicleSpeed"):
+        super(ChangeVehicleSpeed, self).__init__(name)
+        self.vehicle = vehicle
+        self.speed = speed
+        self.duration = duration
+        self.start_time = None
+
+    def initialise(self):
+        self.start_time = None
+
+    def update(self):
+        if self.vehicle is None:
+            return py_trees.common.Status.FAILURE
+
+        if self.start_time is None:
+            self.start_time = self.vehicle.get_world().get_snapshot().timestamp.elapsed_seconds
+
+        current_time = self.vehicle.get_world().get_snapshot().timestamp.elapsed_seconds
+        elapsed_time = current_time - self.start_time
+
+        if elapsed_time < self.duration:
+            self.vehicle.set_target_velocity(carla.Vector3D(self.speed, 0, 0))
+            return py_trees.common.Status.RUNNING
+        else:
+            self.vehicle.set_target_velocity(carla.Vector3D(0, 0, 0))
+            return py_trees.common.Status.SUCCESS
+
+    def terminate(self, new_status):
+        if self.vehicle is not None:
+            self.vehicle.set_target_velocity(carla.Vector3D(0, 0, 0))
